d0ffcf9447950fda7b278a57190c5a161f086f3a
MauroDS <mauro.diaz.santilli@gmail.com>
2023-02-23 19:35:40 -0300

    agregando los archivos

---
+++ js/1-variables.js
@@ -0,0 +1,34 @@
+// Esto es un comentario de una sola linea
+// Comentario 1
+// Comentario 2
+// Comentario 3
+
+/* Esto es un comentario
+de
+multiples lineas
+*/
+
+// mostrar comentarios o documentos en el doc .html
+document.write("<h1>Hola mundo</h1>");
+
+// mostrar un mensaje en consola
+console.log("Prueba desde la consola");
+
+// mostrar un mensaje en una ventana emergente
+alert("Prueba de alert");
+
+// crear o declarar variables (var - let - const)
+
+let producto = "ipad pro 2021";
+
+document.write(producto);
+
+// modificar e contenido de una variable
+producto = "ipad pro 2023";
+
+document.write("<br>" + producto);
+
+let numero1 = 30
+let numero2 = 50
+
+document.write("<br>" + (numero1+numero2))
