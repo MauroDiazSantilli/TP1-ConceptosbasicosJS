f6819ab8f5efdbce7bba20a3fec9e2b61657263f
MauroDS <mauro.diaz.santilli@gmail.com>
2023-03-01 14:10:05 -0300

    Corrigiendo detalles

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -1,6 +1,6 @@
 let numero = parseInt(prompt("Ingrese un numero"));
 
-if (numero % 2 == 0 || numero % 3 == 0 || numero % 5 == 0 || numero % 5 == 0 || numero % 7 == 0){
+if (numero % 2 == 0 || numero % 3 == 0 || numero % 5 == 0 || numero % 7 == 0){
     document.write(numero + " es divisible por 2, 3, 5 o 7");
 }
 
