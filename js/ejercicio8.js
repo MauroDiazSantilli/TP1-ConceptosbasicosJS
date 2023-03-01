3abe514d55fe27cb93b32110827fc50cfcd47ebf
MauroDS <mauro.diaz.santilli@gmail.com>
2023-03-01 09:26:11 -0300

    Terminando octavo ejercicio

---
+++ js/ejercicio8.js
@@ -0,0 +1,8 @@
+let numero = parseInt(prompt("Ingresá un numero"));
+
+if (numero % 2 == 0){
+    document.write(numero + " es divisible por 2")
+}
+else{
+    document.write(numer + " no es divisible por 2")
+}
